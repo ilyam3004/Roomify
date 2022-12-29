07d2bf1d4e675028a1776f2d53666ebfbd8b7a4b
ilyam3004 <ilyamelnichuk3004@gmail.com>
2022-12-29 23:46:27 +0200

    Adding queries and configuring DependencyInjection

--- src/ChatApp.Infrastructure/Persistence/MessageRepository.cs
+++ src/ChatApp.Infrastructure/Persistence/MessageRepository.cs
@@ -3,6 +3,8 @@ using ChatApp.Infrastructure.Config;
 using ChatApp.Application.Common.Interfaces.Persistence;
 using Dapper;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using ChatApp.Application.Services;
 
 namespace ChatApp.Infrastructure.Persistence;
 
@@ -15,31 +17,44 @@ public class MessageRepository : IMessageRepository
         _dbContext = dbContext;
     }
 
-    public Task<Message> SaveMessage(Message message)
+    public async Task<Message> SaveMessage(Message message)
     {
         var query = "INSERT INTO Message (MessageId, UserId, RoomId, Text, Date, FromUser) " +
                     "VALUES (@MessageId, @UserId, @RoomId, @Text, @Date, @FromUser)";
 
         var parameters = new DynamicParameters();
-        parameters.Add("UserId", user.UserId, DbType.String);
-        parameters.Add("Username", user.Username, DbType.String);
-        parameters.Add("ConnectionId", user.ConnectionId, DbType.String);
-        parameters.Add("RoomId", user.RoomId, DbType.String);
+        parameters.Add("MessageId", message.MessageId, DbType.String);
+        parameters.Add("UserId", message.UserId, DbType.String);
+        parameters.Add("RoomId", message.RoomId, DbType.String);
+        parameters.Add("Text", message.Text, DbType.String);
+        parameters.Add("Date", message.Date, DbType.DateTime);
+        parameters.Add("FromUser", message.FromUser, DbType.Boolean);
 
         using (var connection = _dbContext.CreateConnection())
         {
             await connection.ExecuteAsync(query, parameters);
         }
 
-        var dbUser = await GetUserById(user.UserId);
+        var dbMessage = await GetMessageById(message.MessageId);
 
         //ADD ERROR HANDLING
-        return dbUser;
+        return dbMessage;
     }
 
-    public Task RemoveMessageById(string messageId)
+    public async Task RemoveMessageById(string messageId)
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<Message> GetMessageById(string messageId)
+    {
+        var query = "SELECT * FROM Message WHERE MessageId = @MessageId";
+
+        using (var connection = _dbContext.CreateConnection())
+        {
+            var message = await connection.QueryFirstOrDefaultAsync<Message>(query, new { messageId });
+            return message;
+        }
     }
 
     public Task RemoveAllMessagesFromRoom(string roomId)
