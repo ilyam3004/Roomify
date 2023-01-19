fc23318078175b7f5e061a114d83db2e7bc151ef
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-01-19 21:56:37 +0200

    Fixing bugs with removing data after user leaving

--- src/ChatApp.Application/Common/Interfaces/Persistence/IUserRepository.cs
+++ src/ChatApp.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -6,7 +6,7 @@ public interface IUserRepository
 {
     Task<User> AddUser(User user);
     Task<User> GetUserById(string userId);
-    Task<Room> CreateRoomIfNotExists(string roomName);
+    Task<Room?> CreateRoomIfNotExists(string roomName);
     Task<Room?> GetRoomById(string roomId);
     Task<List<User>> GetRoomUsers(string roomId);
     Task<bool> UserExists(string username, string roomName);
