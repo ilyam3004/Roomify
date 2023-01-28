9c85600592a14de3cf7c243b92064540fc7c03eb
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-01-28 22:42:37 +0200

    Adding unit tests and fixing UserService

--- tests/ChatApp.Application.Tests/Services/UserServiceTests.cs
+++ tests/ChatApp.Application.Tests/Services/UserServiceTests.cs
@@ -1,8 +1,8 @@
 using ChatApp.Application.Common.Interfaces.Persistence;
 using ChatApp.Application.Models.Requests;
-using ChatApp.Application.Models.Responses;
 using ChatApp.Application.Services;
 using ChatApp.Domain.Entities;
+using ErrorOr;
 using FluentValidation;
 using Moq;
 
@@ -13,14 +13,14 @@ public class UserServiceTests
     private readonly UserService _userService;
     private readonly Mock<IUserRepository> _userRepositoryMock = new Mock<IUserRepository>();
     private readonly Mock<IValidator<CreateUserRequest>> _userValidation = new Mock<IValidator<CreateUserRequest>>();
-
+                    
     public UserServiceTests()
     {
         _userService = new UserService(_userRepositoryMock.Object, _userValidation.Object);
     }
 
     [Fact]
-    public async Task GetUserByConnectionId_ShouldReturn_WhenUserExists()
+    public async Task GetUserByConnectionId_ShouldReturn_WhenUserAndRoomExists()
     {
         //Arrange
         var connectionId = Guid.NewGuid().ToString();
@@ -50,5 +50,47 @@ public class UserServiceTests
         
         //Assert
         Assert.Equal(userResponse.Value.ConnectionId, user.ConnectionId);
+    }
+
+    [Fact]
+    public async Task GetUserByConnectionId_ShouldReturnError_WhenUserNotExists()
+    {
+        //Arrange
+        var connectionId = Guid.NewGuid().ToString();
+        var user = new User
+        {
+            UserId = Guid.NewGuid().ToString(),
+            Username = "username",
+            ConnectionId = connectionId,
+            RoomId = Guid.NewGuid().ToString(),
+            HasLeft = false
+        };
+        _userRepositoryMock.Setup(x => x.GetUserByConnectionIdOrNull(connectionId))
+            .ReturnsAsync(null as User);
+
+        var roomId = user.RoomId;
+        var room = new Room
+        {
+            RoomId = user.RoomId,
+            RoomName = "RoomName"
+        };
+        _userRepositoryMock.Setup(x => x.GetRoomById(roomId))
+            .ReturnsAsync(room);
+        
+        //Act
+        var userResponse = await _userService
+            .GetUserByConnectionId(connectionId);
+        
+        //Assert
+        Assert.Equal(ErrorType.Conflict, userResponse.FirstError.Type);
+    }
+
+    public async Task JoinRoom_ShouldReturnObject_IfRequestValid()
+    {
+        //Arrange
+        
+        //Act
+        
+        //Assert
     }
 }
