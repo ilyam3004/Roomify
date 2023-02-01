f05105b198cfa87ce23ef7bcdd06b54d9d689bba
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-01 21:11:15 +0200

    Testing library for unit testing SignalR hubs

--- tests/ChatApp.Api.Tests/Hubs/ChatHubTests.cs
+++ tests/ChatApp.Api.Tests/Hubs/ChatHubTests.cs
@@ -1,17 +1,47 @@
-﻿using ChatApp.Api.Hubs;
-using ChatApp.Application.Services;
+﻿using ChatApp.Application.Services;
+using ChatApp.Api.Hubs;
+using AutoFixture;
 using Moq;
+using ErrorOr;
+using ChatApp.Contracts.Rooms;
+using ChatApp.Application.Models.Requests;
+using SignalR_UnitTestingSupportXUnit.Hubs;
+using ChatApp.Application.Models.Responses;
+using ChatApp.Domain.Common.Errors;
 
 namespace ChatApp.Api.Tests.Hubs;
-public  class ChatHubTests
+public  class ChatHubTests : HubUnitTestsBase
 {
     private readonly ChatHub _sut;
-    private readonly Mock<IUserService> _userService = new Mock<IUserService>();
-    private readonly Mock<IMessageService> _messageService = new Mock<IMessageService>();
+    private readonly Mock<IUserService> _userServiceMock = new();
+    private readonly Mock<IMessageService> _messageServiceMock = new();
+    private readonly Fixture _fixture;
 
     public ChatHubTests() 
     {
-        _sut = new ChatHub(_userService.Object, _messageService.Object);
-    }   
+        _sut = new ChatHub(_userServiceMock.Object, _messageServiceMock.Object);
+        AssignToHubRequiredProperties(_sut);
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public async Task Test()
+    {
+        var joinUserRequest = _fixture.Create<JoinUserRequest>();
+
+        var userResponse = _fixture.Create<UserResponse>();
+
+        _userServiceMock
+            .Setup(a => a.AddUserToRoom(It.IsAny<CreateUserRequest>()))
+            .ReturnsAsync(userResponse);
+        
+        _messageServiceMock
+            .Setup(x => x.SaveMessage(It.IsAny<SaveMessageRequest>()))
+            .ReturnsAsync(_fixture.Create<MessageResponse>());
+
+        await _sut.JoinRoom(joinUserRequest);
+        
+        _userServiceMock.Verify(u => u.AddUserToRoom(It.IsAny<CreateUserRequest>()), Times.Once);
+    }
 }
 
