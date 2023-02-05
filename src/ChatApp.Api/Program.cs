bc664545f15627b071afb8dbdf877f58ee5c40b9
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-05 17:35:04 +0200

    Testing signalR without azure

--- src/ChatApp.Api/Program.cs
+++ src/ChatApp.Api/Program.cs
@@ -8,25 +8,26 @@ var builder = WebApplication.CreateBuilder(args);
     .AddInfrastructure()
     .AddApplication()
     .AddControllers();
-    
+
     builder.Services
         .AddCors()
-        .AddSignalR()
-        .AddAzureSignalR("Endpoint=https://chat-app-server.service.signalr.net;AccessKey=NBgFpQSN4/PM4qewDJx8f0ujMbIoteI6zInoRhAhxNw=;Version=1.0;");
+        .AddSignalR();
+    //.AddAzureSignalR("Endpoint=https://chat-app-server.service.signalr.net;AccessKey=NBgFpQSN4/PM4qewDJx8f0ujMbIoteI6zInoRhAhxNw=;Version=1.0;");
 }
 
 var app = builder.Build();
 {
     app.UseCors(builder => builder
-            .AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowCredentials()
-            .AllowAnyMethod()
-            .WithOrigins("http://localhost:3000"));
-    
-    app.UseAzureSignalR(routes =>
-    {
-        routes.MapHub<ChatHub>("/chatHub");
-    });
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowCredentials()
+        .SetIsOriginAllowed((host) => true)
+        .AllowAnyMethod());
+
+    // app.UseAzureSignalR(routes =>
+    // {
+    //     routes.MapHub<ChatHub>("/chatHub");
+    // });
+    app.MapHub<ChatHub>("/chatHub");
     app.Run();
 }
