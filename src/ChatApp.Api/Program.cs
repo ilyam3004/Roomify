1baf2a7ae08109349028e32ca150b98b403a9fbf
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-05 20:05:18 +0200

    Update client url for cors policy credentials

--- src/ChatApp.Api/Program.cs
+++ src/ChatApp.Api/Program.cs
@@ -12,21 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddCors()
         .AddSignalR();
-    //.AddAzureSignalR("Endpoint=https://chat-app-server.service.signalr.net;AccessKey=NBgFpQSN4/PM4qewDJx8f0ujMbIoteI6zInoRhAhxNw=;Version=1.0;");
 }
 
 var app = builder.Build();
 {
     app.UseCors(builder => builder
-        .AllowAnyHeader()
+        .AllowAnyOrigin()
         .AllowCredentials()
-        .SetIsOriginAllowed((host) => true)
-        .AllowAnyMethod());
-
-    // app.UseAzureSignalR(routes =>
-    // {
-    //     routes.MapHub<ChatHub>("/chatHub");
-    // });
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("https://chat-app-psi-ruby.vercel.app"));
+    
     app.MapHub<ChatHub>("/chatHub");
     app.Run();
 }
