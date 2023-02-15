bf8f216135950679efc8a5fb107d82a28600ff45
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-15 23:50:23 +0200

    Implementing Mapster

---
+++ src/ChatApp.Api/Common/Mapping/ImageMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using ChatApp.Contracts.Rooms.Responses;
+using CloudinaryDotNet.Actions;
+using Mapster;
+
+namespace ChatApp.Api.Common.Mapping;
+
+public class ImageMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<ImageUploadResult, UploadResultResponse>()
+            .Map(dest => dest.ImgUrl, src => src.Url);
+    }
+}
